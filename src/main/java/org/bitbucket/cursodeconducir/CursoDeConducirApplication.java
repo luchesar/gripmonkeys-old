bec566094a358a978ddcb9beacc50547a5058c37
Luchesar Cekov <luchesar@gmail.com>
2011-04-29 17:24:19 +0100

    making the build better

---
+++ src/main/java/org/bitbucket/cursodeconducir/CursoDeConducirApplication.java
@@ -0,0 +1,36 @@
+package org.bitbucket.cursodeconducir;
+
+import org.vaadin.addon.borderlayout.BorderLayout;
+
+import com.vaadin.Application;
+import com.vaadin.incubator.dashlayout.ui.VerDashLayout;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
+
+/**
+ * The Application's "main" class
+ */
+@SuppressWarnings("serial")
+public class CursoDeConducirApplication extends Application {
+	private Window window;
+
+	@Override
+	public void init() {
+		window = new Window("My Vaadin Application");
+		setMainWindow(window);
+		
+		
+		VerDashLayout root = new VerDashLayout();
+		root.setSizeFull();
+		root.setMargin(true);
+		root.setSpacing(true);
+		root.setStyleName("root");
+		window.setContent(root);
+		
+		setTheme("cursodeconducir");
+
+	}
+
+}
