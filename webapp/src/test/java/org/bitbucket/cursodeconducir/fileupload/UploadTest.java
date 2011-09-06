c5ff50e367b82b421e256272c4a7f575dfabb29b
Luchesar Cekov <luchesar@gmail.com>
2011-09-06 08:32:08 +0100

    modularizing the project

--- webapp/src/test/java/org/bitbucket/cursodeconducir/fileupload/UploadTest.java
+++ webapp/src/test/java/org/bitbucket/cursodeconducir/fileupload/UploadTest.java
@@ -6,13 +6,21 @@ import org.bitbucket.cursodeconducir.services.fileupload.Upload;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.ontologypartners.mocks.servlet.MockHttpServletRequest;
+import com.ontologypartners.mocks.servlet.MockHttpServletResponse;
+
 public class UploadTest {
     private Upload upload;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
 
 
     @Before
     public void setUp() throws Exception {
         upload = new Upload();
+        request = new MockHttpServletRequest();
+        request.setProtocol("HTTP/1.1");
+        response = new MockHttpServletResponse();
     }
     
     @Test
