d0e5a5168fdbd82f902f74cc2fe381baba1ad32f
Luchesar Cekov <luchesar@gmail.com>
2011-09-19 09:06:49 +0100

    updating projects class path to support proper unit testing

--- webapp/src/test/java/org/bitbucket/cursodeconducir/fileupload/BlobServeTest.java
+++ webapp/src/test/java/org/bitbucket/cursodeconducir/fileupload/BlobServeTest.java
@@ -1,7 +1,8 @@
 package org.bitbucket.cursodeconducir.fileupload;
 
-
 import static junit.framework.Assert.*;
+
+import java.io.File;
 
 import org.bitbucket.cursodeconducir.mocks.MockHttpServletRequest;
 import org.bitbucket.cursodeconducir.mocks.MockHttpServletResponse;
@@ -13,41 +14,45 @@ import org.junit.Test;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.tools.development.testing.LocalBlobstoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 public class BlobServeTest {
-    private LocalBlobstoreServiceTestConfig config = new LocalBlobstoreServiceTestConfig();
+    private final LocalServiceTestHelper helper =
+        new LocalServiceTestHelper(new LocalBlobstoreServiceTestConfig());
     private BlobServe blobServe;
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
     private BlobstoreService blobstoreService;
-    
+
     @Before
     public void setUp() throws Exception {
-        config.setUp();
+        helper.setUp();
+        String uploadUrl = blobstoreService.createUploadUrl("/");
         blobServe = new BlobServe();
-         blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+        blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
         request = new MockHttpServletRequest();
         request.setProtocol("HTTP/1.1");
+        request.setPathInfo(uploadUrl);
         response = new MockHttpServletResponse();
     }
-    
+
     @After
     public void tearDown() throws Exception {
-        config.tearDown();
+        helper.tearDown();
     }
-    
+
     @Test
     public void testDownload() throws Exception {
-        String uploadUrl = blobstoreService.createUploadUrl("/");
+        
         blobServe.service(request, response);
     }
-    
+
     @Test
     public void testServeMissingFile() throws Exception {
         fail();
-        
+
     }
-    
+
     @Test
     public void testWrongRquest() throws Exception {
         fail();
