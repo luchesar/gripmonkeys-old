2da13347cdb0abd4abb5bcd1928aa36c799880e8
Luchesar Cekov <luchesar@gmail.com>
2012-08-04 08:40:28 +0100

    factoring out the TitledEntityServlet
    --HG--
    branch : closure-compiler

--- webapp/src/main/java/org/bitbucket/cursodeconducir/services/LessonStorageServlet.java
+++ webapp/src/main/java/org/bitbucket/cursodeconducir/services/LessonStorageServlet.java
@@ -1,128 +1,18 @@
 package org.bitbucket.cursodeconducir.services;
 
-import java.io.IOException;
-import java.util.Set;
+import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.io.IOUtils;
 import org.bitbucket.cursodeconducir.services.entity.Lesson;
 import org.bitbucket.cursodeconducir.services.storage.LessonStorage;
 
-import com.google.common.collect.Lists;
-import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @SuppressWarnings("serial")
-public class LessonStorageServlet extends HttpServlet {
-	public static final String JSON_KEY = "json";
-	public static final String ID = "key";
-	public static final String OFFSET = "offset";
-	public static final String LENGTH = "length";
-	public static final String INVALID_JSON = "passed test JSON is not valid:";
-	private Gson gson;
-	private LessonStorage storage;
+public class LessonStorageServlet extends
+		TitledEntityStorageServlet<LessonStorage, Lesson> {
 
 	public LessonStorageServlet() {
-		gson = new Gson();
-		storage = new LessonStorage();
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest aReq, HttpServletResponse aResp)
-			throws ServletException, IOException {
-		setResponseEnconding(aResp);
-		String all = aReq.getParameter("*");
-		String lessonIds = aReq.getParameter(ID);
-		String offset = aReq.getParameter(OFFSET);
-		String length = aReq.getParameter(LENGTH);
-		
-		if (all != null) {
-			gson.toJson(storage.getAll(), aResp.getWriter());
-		} else if (lessonIds != null){
-			long[] ids = getIdArray(lessonIds);
-			Set<Lesson> foundQuestions = storage.get(ids);
-			if (foundQuestions.isEmpty()) {
-				aResp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			} else if (foundQuestions.size() == 1) {
-				gson.toJson(foundQuestions.iterator().next(), aResp.getWriter());
-			} else {
-				gson.toJson(foundQuestions, aResp.getWriter());
-			}
-		} else if (offset != null) {
-			int offsetI = Integer.parseInt(offset);
-			int lengthI = Integer.MAX_VALUE;
-			if (length != null) {
-				lengthI = Integer.parseInt(length);
-				if (lengthI < 0) {
-					lengthI = Integer.MAX_VALUE;
-				}
-			}
-			if (offsetI < 0) {
-				aResp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				aResp.getWriter().write("OFFSET parameter must not be negative");
-				return;
-			}
-			gson.toJson(storage.getAll(offsetI, lengthI), aResp.getWriter());
-		}
-	}
-
-	private long[] getIdArray(String idString) {
-		String[] idStrings = idString.split(",");
-		long[] ids = new long[idStrings.length];
-		for (int i = 0; i < idStrings.length; i++) {
-			ids[i] = Long.parseLong(idStrings[i]);
-		}
-		return ids;
-	}
-
-	@Override
-	protected void doPut(HttpServletRequest aReq, HttpServletResponse aResp)
-			throws ServletException, IOException {
-		doPost(aReq, aResp);
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest aReq, HttpServletResponse aResp)
-			throws ServletException, IOException {
-		setResponseEnconding(aResp);
-		java.util.List<Lesson> questionsToStore = Lists.newArrayList();
-		String lessonsJson = aReq.getParameter(JSON_KEY);
-		if (lessonsJson == null) {
-			aResp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			IOUtils.write(INVALID_JSON + lessonsJson, aResp.getOutputStream());
-			return;
-		}
-		Lesson[] deserializedQuestions = gson.fromJson(lessonsJson,
-				new Lesson[0].getClass());
-		questionsToStore.addAll(Lists.newArrayList(deserializedQuestions));
-		try {
-			Set<Lesson> put = storage.put(questionsToStore
-					.toArray(new Lesson[0]));
-			aResp.getWriter().write(gson.toJson(put));
-			aResp.setStatus(HttpServletResponse.SC_CREATED);
-		} catch (ServiceException e) {
-			aResp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-	}
-
-	@Override
-	protected void doDelete(HttpServletRequest aReq, HttpServletResponse aResp)
-			throws ServletException, IOException {
-		setResponseEnconding(aResp);
-		long[] ids = getIdArray(aReq.getParameter(ID));
-		storage.delete(ids);
-		aResp.getWriter().write(gson.toJson(true));
-	}
-
-	public LessonStorage getStorage() {
-		return storage;
-	}
-
-	private void setResponseEnconding(HttpServletResponse aResp) {
-		aResp.setContentType("application/json; charset=utf-8");
-		aResp.setCharacterEncoding("UTF-8");
+		super(new LessonStorage(), new TypeToken<List<Lesson>>() {
+		});
 	}
 }
