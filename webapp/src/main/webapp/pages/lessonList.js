997d264e91eddc403a3da307a9c7dc76a515ef11
Luchesar Cekov <luchesar@gmail.com>
2012-08-11 12:49:39 +0100

    adding even more annotations for the compiler
    --HG--
    branch : closure-compiler

--- webapp/src/main/webapp/pages/lessonList.js
+++ webapp/src/main/webapp/pages/lessonList.js
@@ -5,16 +5,22 @@ goog.require('goog.array');
 
 /**
  * @constructor
- * @param {Object}
- *            container
+ * @param {jQuery} container
  */
 cursoconducir.index.LessonList = function(container) {
+	/**
+	 * @public
+	 * @param {Array.<cursoconducir.Lesson>} allLessons
+	 * @returns {jQuery}
+	 */
 	this.show = function(allLessons) {
+		/** @type {string}*/
 		var allLessonsHtml = cursoconducir.lessonlist.template.lessonList({
 			allLessons : allLessons
 		});
 		container.empty();
 		container.append(allLessonsHtml);
+		/** @type {jQuery}*/
 		var lessonListContainer = container.find('#lessonListContainer');
 		return lessonListContainer;
 	};
